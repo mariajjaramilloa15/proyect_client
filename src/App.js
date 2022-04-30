a2e73addc6f6bcec1414298e271310af54f3a97a
Maria Jose Jaramillo A <mariaj.jaramilloa@autonoma.edu.co>
2022-04-30 16:31:35 -0500

    Proyecto Cliente con react y creación de pagina Not Found

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { BrowserRouter as Router, Route, Link, BrowserRouter, Routes } from "react-router-dom";
+import React from "react";
+import routes from "./config/routes";
+import error404 from "./pages/error404";
 
-function App() {
+
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Router>
+      <Routes>
+        {routes.map((route, index, error404) => (
+          <Route
+            key={index}
+            path={route.path}
+            component={error404}
+            element={
+              <route.layout>
+                <route.component>
+                <h1>Error 404</h1>
+                <p>Not Found</p>
+                </route.component>
+                <h2>Child component</h2>
+              </route.layout>
+            }
+          />
+        ))}
+      </Routes>
+    </Router>
     </div>
   );
 }
-
-export default App;
---
+++ src/pages/error404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const error404 = () => {
+  return (
+    <div>
+        <h1>Error 404</h1>
+        <p>Not Found</p>
+    </div>
+  )
+}
+
+export default error404;
