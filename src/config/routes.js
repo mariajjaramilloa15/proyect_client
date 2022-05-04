c06b35ecda21c16332b78235a258b8d495882b4b
Maria Jose Jaramillo A <mariaj.jaramilloa@autonoma.edu.co>
2022-05-04 13:45:39 -0500

    Se pusieron estilos a las páginas

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,14 @@
 import LayoutAdmin from "../layouts/LayoutAdmin";
-import LayoutError404 from "../layouts/LayoutError404";
+import LayoutBasic from "../layouts/LayoutBasic";
+/* Importamos las pages */
 import AdminHome from "../pages/Admin";
 import AdminSignIn from "../pages/Admin/signIn";
+import Home from "../pages/home"
+import LayoutError404 from "../layouts/LayoutError404";
+import Contact from "../pages/contact"
 import error404 from "../pages/error404";
 
-const routes = [
+const routesAdmin = [
     {
         path: "/admin",
         layout: LayoutAdmin,
@@ -14,12 +18,29 @@ const routes = [
         path: "/admin/login",
         layout: LayoutAdmin,
         component: AdminSignIn,
+    }
+];
+
+const routesClient = [
+    {
+        path: "/",
+        layout: LayoutBasic,
+        component: Home,
     },
+    {
+        path: "/admin/login",
+        layout: LayoutAdmin,
+        component: AdminSignIn,
+    },
+];
+
+const routeNotFound = [
     {
         path: "*",
         layout: LayoutError404,
         component: error404,
-        },
+    },
 ];
 
+const routes = [...routesAdmin, ...routesClient, ...routeNotFound];
 export default routes;
--- src/layouts/LayoutAdmin.js
+++ src/layouts/LayoutAdmin.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { Layout } from "antd";
+import MenuTop from "../components/AdminComponents/MenuTop";
+import "./LayoutAdmin.scss";
 
-export default function LayoutAdmin(props){
-    const { children } = props;
-    return (
-        <Layout>
-            <h2>Menu Sider</h2>
-            <div>Contenido...</div>
-            <h5>Footer...</h5>
-            {children}
-        </Layout>
-    );
+export default function LayoutAdmin(props) {
+  const { children } = props;
+
+  //Especificas los componentes que quieres obtener de este layout
+  const { Header, Content, Footer } = Layout;
+  return (
+    <Layout>
+      <Layout className="layout-admin">
+        <Header className="layout-admin_header">
+          <MenuTop></MenuTop>
+        </Header>
+        <Content className="layout-admin_content">{children}</Content>
+        <Footer className="layout-admin_footer">React Project 2022</Footer>
+      </Layout>
+    </Layout>
+  );
 }
