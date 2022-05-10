5fdaa239885a40650fb86dd57b80d7fa254f5afa
Maria Jose Jaramillo A <mariaj.jaramilloa@autonoma.edu.co>
2022-05-10 13:04:48 -0500

    Se le dio un orden al diseño en el menu y al logo del proyecto

--- src/layouts/LayoutAdmin.js
+++ src/layouts/LayoutAdmin.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout } from "antd";
 import MenuTop from "../components/AdminComponents/MenuTop";
+import MenuSider from "../components/AdminComponents/MenuSider";
 import "./LayoutAdmin.scss";
 
 export default function LayoutAdmin(props) {
-  const { children } = props;
-
-  //Especificas los componentes que quieres obtener de este layout
+  const [menuCollapsed, setMenuCollapsed] = useState(false);
   const { Header, Content, Footer } = Layout;
-  return (
+
+  return(
     <Layout>
+      <MenuSider menuCollapsed={menuCollapsed} />
       <Layout className="layout-admin">
-        <Header className="layout-admin_header">
-          <MenuTop></MenuTop>
+        <Header className="layout-admin__header">
+          <MenuTop 
+            menuCollapsed = {menuCollapsed}
+            setMenuCollapsed = {setMenuCollapsed}
+          />
         </Header>
-        <Content className="layout-admin_content">{children}</Content>
-        <Footer className="layout-admin_footer">React Project 2022</Footer>
+        <Content className="layout-admin__content">
+          <h1>Rutas</h1>
+        </Content>
+        <Footer className="layout-admin__footer">MERN REACT</Footer> 
       </Layout>
     </Layout>
   );
