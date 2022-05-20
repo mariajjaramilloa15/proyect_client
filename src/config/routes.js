0af65c379d6b6fb0c8d64ae9e2cced8b4fbe801c
Maria Jose Jaramillo A <mariaj.jaramilloa@autonoma.edu.co>
2022-05-20 11:05:32 -0500

    Se actualizo la Validez de los datos a ser ingresado

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,42 +3,42 @@ import LayoutBasic from "../layouts/LayoutBasic";
 /* Importamos las pages */
 import AdminHome from "../pages/Admin";
 import AdminSignIn from "../pages/Admin/SignIn/SignIn";
-import Home from "../pages/home"
+import Home from "../pages/home";
 import LayoutError404 from "../layouts/LayoutError404";
 import error404 from "../pages/error404";
 
 const routesAdmin = [
-    {
-        path: "/admin",
-        layout: LayoutAdmin,
-        component: AdminHome,
-    },
-    {
-        path: "/admin/login",
-        layout: LayoutAdmin,
-        component: AdminSignIn,
-    }
+  {
+    path: "/admin",
+    layout: LayoutAdmin,
+    component: AdminHome,
+  },
+  {
+    path: "/admin/login",
+    layout: LayoutAdmin,
+    component: AdminSignIn,
+  },
 ];
 
 const routesClient = [
-    {
-        path: "/",
-        layout: LayoutBasic,
-        component: Home,
-    },
-    {
-        path: "/admin/login",
-        layout: LayoutAdmin,
-        component: AdminSignIn,
-    },
+  {
+    path: "/",
+    layout: LayoutBasic,
+    component: Home,
+  },
+  {
+    path: "/admin/login",
+    layout: LayoutAdmin,
+    component: AdminSignIn,
+  },
 ];
 
 const routeNotFound = [
-    {
-        path: "*",
-        layout: LayoutError404,
-        component: error404,
-    },
+  {
+    path: "*",
+    layout: LayoutError404,
+    component: error404,
+  },
 ];
 
 const routes = [...routesAdmin, ...routesClient, ...routeNotFound];
