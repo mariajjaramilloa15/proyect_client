d3cbbc807de524640af39805123f366c4f86207b
Maria Jose Jaramillo A <mariaj.jaramilloa@autonoma.edu.co>
2022-05-20 12:14:40 -0500

    corrección en codigo

--- src/validations/FormValidations.js
+++ src/validations/FormValidations.js
@@ -1,7 +1,7 @@
-/*Minimo de caracteres permitidos en el campo de contraseña, nombre y apellido */
 export function minLengthValidation(inputData, minLength) {
-  const { value } = inputData;
+  const { value } = inputData; 
   removeClassErrorSuccess(inputData);
+
   if (value.length >= minLength) {
     inputData.classList.add("success");
     return true;
@@ -12,10 +12,12 @@ export function minLengthValidation(inputData, minLength) {
 }
 
 export function emailValidation(inputData) {
-  const emailValid =
-    /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/;
+  // eslint-disable-next-line no-useless-escape
+  const emailValid = /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/;
   const { value } = inputData;
+
   removeClassErrorSuccess(inputData);
+
   const resultValidation = emailValid.test(value);
   if (resultValidation) {
     inputData.classList.add("success");
